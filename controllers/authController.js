50cc0f2df39f80bb154db8825b8ca4e28c82b369
DirhThi <20520771@gm.uit.edu.vn>
2023-12-21 23:05:54 +0700

    routes , auth controller update

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,6 @@ const User = require("../model/user");
 const ResError = require("../utils/ResError");
 
 
-
 const authController = {
 
 // [POST] auth/register
@@ -11,10 +10,10 @@ const authController = {
         const { name, email, password } = req.body;
 
         // Check if user with email already exists
-        // const userExists = await User.findOne({ email });
-        // if (userExists) {
-        //     throw new ResError(400, 'Email đã tồn tại trong hệ thống!');
-        // }
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            throw new ResError(400, 'Email đã tồn tại trong hệ thống!');
+        }
 
         // Create a new user
         const newUser = new User({ name, email, password});
@@ -28,36 +27,34 @@ const authController = {
         next(error);
     }
     },
+
+    // [POST] auth/login
+         login : async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+
+        // Find the user by email
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new ResError(401, 'Email không chính xác');
+        }
+        console.log(user.password , password)
+        // Check if the password is correct
+        const isPasswordValid = password === user.password;
+        if (!isPasswordValid) {
+            console.log(isPasswordValid);
+            throw new ResError(401, 'Mật khẩu không chính xác');
+        }
+
+        const resUser = user.toObject();
+        delete resUser.password;
+        res.status(200).json({
+            ...resUser,
+        });
+    } catch (error) {
+        next(error);
+    }
+},
 };
 
 module.exports = authController;
-// [POST] auth/login
-// export const login = async (req, res, next) => {
-//     try {
-//         const { email, password } = req.body;
-
-//         // Find the user by email
-//         const user = await User.findOne({ email });
-//         if (!user) {
-//             throw new ResError(401, 'Email hoặc mật khẩu không chính xác');
-//         }
-
-//         // Check if the password is correct
-//         const isPasswordValid = await bcrypt.compare(password, user.password);
-//         if (!isPasswordValid) {
-//             throw new ResError(401, 'Email hoặc mật khẩu không chính xác');
-//         }
-
-//         // Generate a JWT token
-//         const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
-
-//         const resUser = user.toObject();
-//         delete resUser.password;
-//         res.status(200).json({
-//             ...resUser,
-//             token,
-//         });
-//     } catch (error) {
-//         next(error);
-//     }
-// };
